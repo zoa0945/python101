6cecb7b7ceeae9507a965d156ad48c03ea949413
zoa0945 <51810980+zoa0945@users.noreply.github.com>
2021-10-19 18:35:45 +0900

    Create 'n'base.py

---
+++ input&output/'n'base.py
@@ -0,0 +1,25 @@
+import sys
+
+# 16진법으로 출력 (영문 소문자로 출력)
+a = sys.stdin.readline().rstrip()
+n = int(a)
+
+print("%x"%n)
+
+# 16진법으로 출력 (영문 대문자로 출력)
+a = sys.stdin.readline().rstrip()
+n = int(a)
+
+print("%X"%n)
+
+# 8진법으로 출력
+a = sys.stdin.readline().rstrip()
+n = int(a)
+
+print("%o"%n)
+
+# 16진법으로 변환하여 8진법으로 출력
+a = sys.stdin.readline().rstrip()
+n = int(a, 16)
+
+print("%o"%n)
